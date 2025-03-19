7ecb61948ed34e973edefad1e2758025c4afe8b4
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-20 01:39:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let text = document.querySelector("#text");
+let delay = document.querySelector("#delay");
+let submitBtn = document.querySelector("#btn");
+let outputMsg = document.querySelector("#output");
+
+submitBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	setTimeout(() => {
+		outputMsg.textContent = text.value;
+	}, delay.value);
+});
